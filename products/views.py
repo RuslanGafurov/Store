28a871b466b10b401057f2849184a09b4fd1f208
RuslanGafurov <g.r.9@mail.ru>
2023-10-21 17:13:34 +0300

    Added home and products views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import Category, Product
+
+
+def home_view(request):
+    return render(request, 'home.html')
+
+
+def products_view(request):
+    context = {
+        'categories': Category.objects.all(),
+        'products': Product.objects.all(),
+    }
+    return render(request, 'products/products.html', context)
