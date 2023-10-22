9c21bf12900dc0470776a3e3d0323999a359b7de
RuslanGafurov <g.r.9@mail.ru>
2023-10-22 15:46:55 +0300

    Added work with media

--- store/urls.py
+++ store/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', include(('products.urls', 'products'))),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
