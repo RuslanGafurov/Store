73507c568cef6f3848e98c438fdac1353f39794c
RuslanGafurov <g.r.9@mail.ru>
2023-10-22 12:31:02 +0300

    Extended models

--- products/models.py
+++ products/models.py
@@ -2,24 +2,30 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+        ordering = ['name']
 
     def __str__(self):
         return str(self.name)
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=256)
     description = models.TextField(null=True, blank=True)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey('products.Category', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
+        ordering = ['name']
 
     def __str__(self):
         return str(self.name)
