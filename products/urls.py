d2b8547c773c18939e9d941b2760259201134380
RuslanGafurov <g.r.9@mail.ru>
2023-10-25 15:08:57 +0300

    Added app_name

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from products.views import HomePageView, ProductsPageView
 
+app_name = 'products'
+
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('products/', ProductsPageView.as_view(), name='products'),
