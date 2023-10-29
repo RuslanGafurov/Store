4f108a83de4b79864feded654dc2ae0c31f019ae
RuslanGafurov <g.r.9@mail.ru>
2023-10-29 14:24:06 +0300

    Added urls for logout and profile

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from users.views import user_login_view, user_registration_view
+from users.views import *
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', user_login_view, name='login'),
+    path('logout/', user_logout_view, name='logout'),
+    path('profile/', user_profile_view, name='profile'),
     path('registration/', user_registration_view, name='registration'),
 ]
