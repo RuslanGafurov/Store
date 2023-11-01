96166a66c825322cf63e709bb4b3f6c0d19d1899
RuslanGafurov <g.r.9@mail.ru>
2023-11-01 13:52:29 +0300

    Added Basket model

--- products/models.py
+++ products/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    category = models.ForeignKey('products.Category', on_delete=models.CASCADE)
+    category = models.ForeignKey(to='products.Category', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Товар'
@@ -31,3 +31,13 @@ class Product(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(to='users.User', on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Корзина для {self.user.email}'
