d82135511e39ab73f5dda82c8a0af6c434e415ce
RuslanGafurov <g.r.9@mail.ru>
2023-11-01 16:23:59 +0300

    Fixed a bug

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', LoginPageView.as_view(), name='login'),
-    path('profile/<int:pk>', ProfilePageView.as_view(), name='profile'),
-    path('registration/', login_required(RegistrationPageView.as_view()), name='registration'),
+    path('profile/<int:pk>', login_required(ProfilePageView.as_view()), name='profile'),
+    path('registration/', RegistrationPageView.as_view(), name='registration'),
     path('logout/', logout_page_view, name='logout'),
 ]
