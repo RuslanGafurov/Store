c712569847f96e54a15d88a79484a8ce4c5fc788
RuslanGafurov <g.r.9@mail.ru>
2023-11-01 13:18:13 +0300

    Extended display of models

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from products.models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Расширенное отображение категорий"""
+    list_display = ('name', 'description')
+    fields = ('name', 'description')
+    ordering = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Расширенное отображение товаров"""
+    list_display = ('name', 'category', 'price', 'quantity')
+    fields = ('name', 'category', 'image', 'description', ('quantity', 'price'))
+    ordering = ('name',)
