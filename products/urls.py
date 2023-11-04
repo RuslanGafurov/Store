f1cb50e51227b43fea7b187c2f881d17a2eb03a3
RuslanGafurov <g.r.9@mail.ru>
2023-11-04 18:43:23 +0300

    Added filtering by category

--- products/urls.py
+++ products/urls.py
@@ -7,6 +7,7 @@ app_name = 'products'
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('products/', ProductsPageView.as_view(), name='products'),
+    path('products/category/<int:category_id>', ProductsPageView.as_view(), name='category'),
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
