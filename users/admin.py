c9025a1270929611f0a62bf357931063477b37df
RuslanGafurov <g.r.9@mail.ru>
2023-11-05 14:47:10 +0300

    Added EmailVerificationAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
 from products.admin import BasketAdmin
-from users.models import User
+from users.models import EmailVerification, User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     inlines = (BasketAdmin,)
+
+
+@admin.register(EmailVerification)
+class EmailVerificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'code', 'expiration')
+    fields = ('code', 'user', 'expiration', 'timestamp')
+    readonly_fields = ('timestamp',)
