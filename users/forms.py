c22f8e389bc7b5463612f11ccf086f9539c8220f
RuslanGafurov <g.r.9@mail.ru>
2023-11-05 14:46:55 +0300

    Extended UserRegistrationForm for sending emails

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,12 @@
+import uuid
+from datetime import timedelta
+
 from django import forms
 from django.contrib.auth.forms import (AuthenticationForm, UserChangeForm,
                                        UserCreationForm)
+from django.utils.timezone import now
 
-from users.models import User
+from users.models import EmailVerification, User
 
 
 class UserLoginForm(AuthenticationForm):
@@ -51,6 +55,17 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        """Отправка письма с кодом для подтверждения почты"""
+        user = super(UserRegistrationForm, self).save(commit=True)
+        record = EmailVerification.objects.create(
+            user=user,
+            code=uuid.uuid4(),
+            expiration=now() + timedelta(hours=48),
+        )
+        record.send_verification_email()
+        return user
 
 
 class UserProfileForm(UserChangeForm):
