9c4dace54509eaa4f88b93e18ec308942eeb4e73
RuslanGafurov <g.r.9@mail.ru>
2023-11-11 12:51:33 +0300

    Renamed

--- products/views.py
+++ products/views.py
@@ -6,20 +6,20 @@ from common.views import TitleMixin
 from products.models import Basket, Category, Product
 
 __all__ = (
-    'HomePageView',
-    'ProductsPageView',
+    'HomeView',
+    'ProductsView',
     'basket_add',
     'basket_remove',
 )
 
 
-class HomePageView(TitleMixin, TemplateView):
+class HomeView(TitleMixin, TemplateView):
     """Отображение домашней страницы"""
     template_name = 'home.html'
     title = 'Store'
 
 
-class ProductsPageView(TitleMixin, ListView):
+class ProductsView(TitleMixin, ListView):
     """Отображение каталога товаров"""
     template_name = 'products/products.html'
     title = 'Store - Каталог'
@@ -27,12 +27,12 @@ class ProductsPageView(TitleMixin, ListView):
     paginate_by = 3
 
     def get_context_data(self, **kwargs):
-        context = super(ProductsPageView, self).get_context_data(**kwargs)
+        context = super(ProductsView, self).get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         return context
 
     def get_queryset(self):
-        queryset = super(ProductsPageView, self).get_queryset()
+        queryset = super(ProductsView, self).get_queryset()
         category_id = self.kwargs.get('category_id')
         return queryset.filter(category_id=category_id) if category_id else queryset
 
