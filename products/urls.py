82821abcf95e5fb363a898d339e5640875eb6654
RuslanGafurov <g.r.9@mail.ru>
2023-11-11 12:51:17 +0300

    Renamed

--- products/urls.py
+++ products/urls.py
@@ -5,9 +5,9 @@ from products.views import *
 app_name = 'products'
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('products/', ProductsPageView.as_view(), name='products'),
-    path('products/category/<int:category_id>', ProductsPageView.as_view(), name='category'),
+    path('', HomeView.as_view(), name='home'),
+    path('products/', ProductsView.as_view(), name='products'),
+    path('products/category/<int:category_id>', ProductsView.as_view(), name='category'),
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
