f7d4e66bb9e71688600a116223901fa9bd1cd3ea
RuslanGafurov <g.r.9@mail.ru>
2023-11-18 18:06:56 +0300

    Added Order model

---
+++ orders/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+from users.models import User
+
+
+class Order(models.Model):
+    """Модель оформления заказа"""
+    CREATED, PAID, ON_WAY, DELIVERED = range(4)
+    STATUSES = (
+        (CREATED, 'Создан'),
+        (PAID, 'Оплачен'),
+        (ON_WAY, 'В пути'),
+        (DELIVERED, 'Доставлен'),
+    )
+
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=256)
+    address = models.CharField(max_length=256)
+    basket_history = models.JSONField(default=dict)
+    created = models.DateTimeField(auto_now_add=True)
+    initiator = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    status = models.SmallIntegerField(default=CREATED, choices=STATUSES)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'Заказ №{self.id} для {self.last_name} {self.first_name}'
+
