57cffcc2e0ab4a8eb63e20715fcfd4fa7227d7b4
RuslanGafurov <g.r.9@mail.ru>
2023-11-19 15:19:35 +0300

    Added success and cancel urls

--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,7 @@ from orders.views import *
 app_name = 'orders'
 
 urlpatterns = [
+    path('success/', SuccessOrderView.as_view(), name='success'),
+    path('cancel/', CancelOrderView.as_view(), name='cancel'),
     path('create/', OrderCreateView.as_view(), name='create'),
 ]
