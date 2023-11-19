5a4a2eb8ee0c90653b3b9e1dc3460a2c52991349
RuslanGafurov <g.r.9@mail.ru>
2023-11-19 15:20:12 +0300

    Added stripe checkout_session, SuccessOrderView and CancelOrderView

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,29 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from http import HTTPStatus
+
+import stripe
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, TemplateView
 
 from common.views import TitleMixin
 from orders.forms import OrderForm
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+__all__ = (
+    'SuccessOrderView',
+    'CancelOrderView',
+    'OrderCreateView',
+)
+
+
+class SuccessOrderView(TitleMixin, TemplateView):
+    template_name = 'orders/success.html'
+    title = 'Store - Спасибо за заказ!'
+
+
+class CancelOrderView(TemplateView):
+    template_name = 'orders/cancel.html'
 
 
 class OrderCreateView(TitleMixin, CreateView):
@@ -11,6 +32,22 @@ class OrderCreateView(TitleMixin, CreateView):
     form_class = OrderForm
     title = 'Store - Оформление заказа'
     success_url = reverse_lazy('orders:create')
+
+    def post(self, request, *args, **kwargs):
+        super(OrderCreateView, self).post(request, *args, **kwargs)
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price': 'price_1OE9i8Du1bbI2E0mBCJb8a9e',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=f"{settings.DOMAIN_NAME}{reverse('orders:success')}",
+            cancel_url=f"{settings.DOMAIN_NAME}{reverse('orders:cancel')}",
+        )
+        return HttpResponseRedirect(checkout_session.url, status=HTTPStatus.SEE_OTHER)
 
     def form_valid(self, form):
         form.instance.initiator = self.request.user
