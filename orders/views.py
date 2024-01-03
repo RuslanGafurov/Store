87fac007debc4e2791e4446033c58fa1e7a07233
Ruslan <g.r.9@mail.ru>
2024-01-03 13:25:17 +0300

    Added stripe_webhook_view and fulfill_order views

--- orders/views.py
+++ orders/views.py
@@ -2,8 +2,9 @@ from http import HTTPStatus
 
 import stripe
 from django.conf import settings
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse_lazy, reverse
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import CreateView, TemplateView
 
 from common.views import TitleMixin
@@ -14,6 +15,7 @@ __all__ = (
     'SuccessOrderView',
     'CancelOrderView',
     'OrderCreateView',
+    'stripe_webhook_view',
 )
 
 
@@ -43,6 +45,7 @@ class OrderCreateView(TitleMixin, CreateView):
                     'quantity': 1,
                 },
             ],
+            metadata={'order_id': self.object.id},
             mode='payment',
             success_url=f"{settings.DOMAIN_NAME}{reverse('orders:success')}",
             cancel_url=f"{settings.DOMAIN_NAME}{reverse('orders:cancel')}",
@@ -52,3 +55,42 @@ class OrderCreateView(TitleMixin, CreateView):
     def form_valid(self, form):
         form.instance.initiator = self.request.user
         return super(OrderCreateView, self).form_valid(form)
+
+
+@csrf_exempt
+def stripe_webhook_view(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET_KEY
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        # Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
+        session = stripe.checkout.Session.retrieve(
+            event['data']['object']['id'],
+            expand=['line_items'],
+        )
+
+        line_items = session.line_items
+        order_id = int(session.metadata.order_id)
+        # Fulfill the purchase...
+        fulfill_order(line_items, order_id)
+
+    # Passed signature verification
+    return HttpResponse(status=200)
+
+
+def fulfill_order(line_items, order_id):
+    print(order_id)
+    print("Fulfilling order")
